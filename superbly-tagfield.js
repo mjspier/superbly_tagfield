06402f82c5ab5413d550def53c4eda7ea431b118
Ryan Caught <rcaught@gmail.com>
2012-11-15 14:10:57 -0500

    Whitespace fixes

--- superbly-tagfield.js
+++ superbly-tagfield.js
@@ -75,11 +75,11 @@
 
         // events
         suggestList.mouseover(function(e){
-            hoverSuggestItems = true; 
+            hoverSuggestItems = true;
         });
 
         suggestList.mouseleave(function(e){
-            hoverSuggestItems = false; 
+            hoverSuggestItems = false;
         });
 
         tagInput.keyup(function(e){
@@ -91,7 +91,7 @@
         tagInput.focusout(function(e){
             if(!hoverSuggestItems){
                 suggestList.css('display', 'none');
-            } 
+            }
         });
 
         tagInput.focus(function(e){
@@ -99,14 +99,14 @@
         });
 
         tagInput.keydown(function(e){
-            if(e.keyCode == keyMap.downArrow) {		
+            if(e.keyCode == keyMap.downArrow) {
                 selectDown();
             }else if(e.keyCode == keyMap.upArrow) {
                 selectUp()
             }else if(e.keyCode == keyMap.enter || e.keyCode == keyMap.tab) {
                 checkForItem();
                 // prevent default action for enter
-                return e.keyCode != keyMap.enter; 
+                return e.keyCode != keyMap.enter;
             }else if(e.keyCode == keyMap.backspace){
                 // backspace
                 if(tagInput.val() == ''){
@@ -126,20 +126,20 @@
 		}
 
         tagList.parent().click(function(e){
-            tagInput.focus();	
+            tagInput.focus();
         });
 
-        // functions 
+        // functions
         function setValue(){
             tagField.val(inserted.join(','));
         }
 
         function updateTagInputWidth()
         {
-            /* 
+            /*
             * To make tag wrapping behave as expected, dynamically adjust
             * the tag input's width to its content's width
-            * The best way to get the content's width in pixels is to add it 
+            * The best way to get the content's width in pixels is to add it
             * to the DOM, grab the width, then remove it from the DOM.
             */
             var temp = $("<span />").text(tagInput.val()).appendTo(inputItem);
@@ -163,15 +163,15 @@
                 }
             }
 		}
-		
-		function addItem(value){  
-			 addItemWithFocus(value,true); 
+
+		function addItem(value){
+			 addItemWithFocus(value,true);
 		}
 
-        function addItemWithFocus(value,setFocusToInputField){          
+        function addItemWithFocus(value,setFocusToInputField){
             var caseInSensitiveFound = false;
             if(!caseSensitive){
-				$.each(inserted,function(index, val) { 
+				$.each(inserted,function(index, val) {
   					if (caseInSensitiveFound == false && (val.toLowerCase() == value.toLowerCase())) {
    						caseInSensitiveFound = true;
    					 	return true;
@@ -189,7 +189,7 @@
                 tagInput.val("");
                 currentValue = null;
                 currentItem = null;
-                // add remove click event 
+                // add remove click event
                 var new_index = tagList.children('.superblyTagItem').size()-1;
                 $(tagList.children('.superblyTagItem')[new_index]).children('a').click(function(e){
                     var value = $($(this).parent('.superblyTagItem').children('span')[0]).text();
@@ -206,12 +206,12 @@
 				disableAddItem()
 			}
         }
-        
+
         function disableAddItem(){
         	tagInput.attr('disabled','disabled');
 			suggestList.css('display','none');
         }
-        
+
         function enableAddItem(){
         	tagInput.removeAttr('disabled');
 			suggestList.show();
@@ -287,7 +287,7 @@
             selectedIndex=null;
             if(!allowNewTags){
                 selectedIndex=0;
-                $(suggestionItems[selectedIndex]).addClass("selected");	
+                $(suggestionItems[selectedIndex]).addClass("selected");
                 currentItem = $(suggestionItems[selectedIndex]).html();
             }
         }
